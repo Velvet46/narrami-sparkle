f55661c8ac9bf040aa799b93b30d7a2eef694157
Gianluca Michelon <advisor.impresa@gmail.com>
2026-07-02 01:43:32 +0200

    feat: logo cliccabile → home

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -19,7 +19,7 @@ export function AppShell({
       <main className="relative z-10 mx-auto w-full max-w-md px-5 pt-4 pb-32">
         {!hideLogo && (
           <div className="mb-2 flex justify-center">
-            <img src={logo} alt="MilleStorie" className="h-16 w-auto drop-shadow-[0_4px_18px_rgba(0,0,0,0.35)]" />
+            <Link to="/"><img src={logo} alt="MilleStorie" className="h-16 w-auto drop-shadow-[0_4px_18px_rgba(0,0,0,0.35)]" /></Link>
           </div>
         )}
         {children}
